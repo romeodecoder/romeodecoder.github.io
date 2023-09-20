47fdae95cf85dad21aa232c8e1d97148ef480aae
romeodecoder <romeodcoder@gmail.com>
2023-09-20 21:54:40 +0100

    sound effect added

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+    apiKey: '47746a2b42e7391d781a30e049887128',
+    // Add other configuration variables here if needed
+};
+
+export default config;
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", () => {
     const locationInput = document.getElementById("locationInput");
     const searchButton = document.getElementById("searchButton");
@@ -9,6 +10,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const humidity = document.getElementById("humidity");
     const windSpeed = document.getElementById("windSpeed");
     const errorMessage = document.getElementById("errorMessage");
+    // Sound effect mapping
+    const soundEffects = {
+        rain: new Audio('WhatsApp Audio 2023-09-20 at 3.38.37 PM.mp3'), // Replace with the path to your rain sound effect file
+        thunderstorm: new Audio('chillout-lounge-downtempo-cool-atmospheric-warm-relaxed-ambient-music-21040.mp3'), // Replace with the path to your thunderstorm sound effect file
+        // Add more sound effects for other weather conditions if needed
+    };
+    let currentSoundEffect = null;
+
 
     // Function to fetch weather data
     async function getWeatherData(location) {
@@ -27,6 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 humidity.textContent = `Humidity: ${data.main.humidity}%`;
                 windSpeed.textContent = `Wind Speed: ${data.wind.speed} km/h`;
                 temperatureUnit.textContent = '\u00B0C';
+                playSoundEffect(data);
             } else {
                 // Display an error message
                 errorMessage.textContent = 'Location not found. Please try again.';
@@ -49,15 +59,54 @@ document.addEventListener("DOMContentLoaded", () => {
         windSpeed.textContent = '';
         temperatureUnit.textContent = '';
     }
+    
+    // Function to play sound effect based on conditions
+    function playSoundEffect(data) {
+        const temperature = data.main.temp.toFixed(1);
+        const humidityValue = data.main.humidity;
+        if (currentSoundEffect) {
+            currentSoundEffect.pause();
+            currentSoundEffect.currentTime = 0;
+            currentSoundEffect = null;
+        }
+
+        if (temperature >0 && temperature <= 100 && humidityValue >= 70) {
+            currentSoundEffect = soundEffects.rain;
+            soundEffects.rain.play();
+        } 
+        else if (temperature > 0 && temperature <= 100 && humidityValue <= 69) {
+            currentSoundEffect = soundEffects.thunderstorm;
+            soundEffects.thunderstorm.play();
+        
+        
+
+        }
+        else{
+            currentSoundEffect = null;
+        }
+        // Add more conditions and sound effects as needed
+    }
+    // Function to stop the current sound effect
+function stopCurrentSoundEffect() {
+    if (currentSoundEffect) {
+        currentSoundEffect.pause();
+        currentSoundEffect.currentTime = 0;
+        currentSoundEffect = null;
+    }
+}
 
     // Event listener for the search button
     searchButton.addEventListener("click", () => {
         const location = locationInput.value.trim();
         if (location !== '') {
             getWeatherData(location);
+            playSoundEffect()
         } else {
             errorMessage.textContent = 'Please enter a location.';
+           
             clearWeatherData();
+             stopCurrentSoundEffect()
         }
     });
+    
 });
